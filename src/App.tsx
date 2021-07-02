9ce626dc95d6e566949e5b5ca462a17237447439
Salik <salik.ansari6@gmail.com>
2021-07-03 01:02:37 +0530

    i give up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React, { useContext } from "react";
 import Kanban from "./components/Kanban";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { v4 as uuid } from "uuid";
-import { data } from "./data";
-import { TaskCardProps } from "./components/TaskCard/index";
 import CardEditModal from "./components/CardEditModal/index";
 import { TasksContext } from "./contexts/TasksContext";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
 
 function App() {
   const { tasks, showModal, currentlyEditing } = useContext(TasksContext);
