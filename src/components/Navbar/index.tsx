635c3871f851368a39250623f69a328a5dc6cb0c
Salik <salik.ansari6@gmail.com>
2021-07-12 16:17:04 +0530

    full navbar with auth functinality done

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,6 +11,11 @@ type User = {
 
 export const Navbar = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+
+  const toggleShowOptions = () => {
+    setShowOptions((prev) => !prev);
+  };
 
   useEffect(() => {
     axios
@@ -21,27 +26,39 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-gray-800 text-white p-3 flex lg:px-20">
+    <nav className="bg-gray-800 text-white p-3 flex lg:px-20 relative">
       <Link to="/" className="flex navbar-brand justify-center items-center">
         <img src="/images/kanban-logo.png" className="h-6 w-6" alt="logo" />
         <h1 className="ml-2 text-2xl">Boardify</h1>
       </Link>
       <div className="links flex ml-auto justify-around items-center w-1/3">
         <Link to="/kanban">Kanban</Link>
-        {user ? (
-          <img
-            src={user.photo}
-            alt="avatar"
-            className="avatar rounded-full h-10 w-10 cursor-pointer"
-          />
-        ) : (
-          <a
-            href="http://localhost:4000/auth/google/"
-            className="bg-gray-700 rounded py-1 px-3"
-          >
-            Login
-          </a>
-        )}
+        <div className="avatar-wrapper relative">
+          {user ? (
+            <img
+              src={user.photo}
+              alt="avatar"
+              onClick={toggleShowOptions}
+              className="avatar rounded-full h-10 w-10 cursor-pointer"
+            />
+          ) : (
+            <a
+              href="http://localhost:4000/auth/google/"
+              className="bg-gray-700 rounded py-1 px-3"
+            >
+              Login
+            </a>
+          )}
+          {showOptions && (
+            <ul className="options w-24 rounded border shadow-md border-gray-700 absolute right-0 p-2 ml bg-white text-gray-900 z-10">
+              <li>
+                <a href="http://localhost:4000/logout" className="">
+                  Log Out
+                </a>
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
     </nav>
   );
