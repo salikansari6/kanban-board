f20d9da71251434b707947ce935125d1d1d186ab
Salik <salik.ansari6@gmail.com>
2021-07-13 18:06:30 +0530

    added purgeCSS whitelist to supoort dynamic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      safelist: [/^bg-gradient-to-\w+/, /^from-\w+/, /^to-\w+/, /^via-\w+/],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
