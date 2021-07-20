b88ab540b4a3d30fe339dc0f92e54d70748ea678
Salik <salik.ansari6@gmail.com>
2021-07-20 21:01:27 +0530

    made homepage responsive for small devices

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -10,21 +10,23 @@ const Homepage = () => {
           autoPlay
           loop
           src="/videos/kanban-video.mp4"
-          className="w-3/4 opacity-20 "
+          className="w-full min-h-screen object-cover lg:w-3/4 opacity-20 "
         ></video>
-        <div className="absolute flex flex-col  justify-center items-center h-full hero-text">
-          <p className="flex items-center mb-5 lg:text-6xl">
+        <div className="absolute flex flex-col pt-24  lg:pt-0 lg:justify-center items-center h-full hero-text">
+          <p className="flex items-center mb-5 text-3xl lg:text-6xl">
             Introducing Boardlia{" "}
             <img
               src="/images/kanban-logo.png"
-              className="h-14 ml-3"
+              className="h-10 lg:h-14 ml-3"
               alt="logo"
             />{" "}
           </p>
-          <p className="text-3xl">Organize all your tasks at a Drag and Drop</p>
+          <p className="text-xl lg:text-3xl">
+            Organize all your tasks at a Drag and Drop
+          </p>
           <Link
             to="/login"
-            className="text-xl bg-gray-800 text-white rounded shadow py-2 px-5 lg:mt-5 border border-gray-800 hover:bg-white hover:text-gray-800"
+            className="text-xl bg-gray-800 text-white rounded shadow py-2 px-5 mt-5 border border-gray-800 hover:bg-white hover:text-gray-800"
           >
             Get started
           </Link>
